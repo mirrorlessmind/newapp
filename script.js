e92c49eade227ddaee8beb48a52883822a87140c
Cigi <cigi@mirrorlessmind.com>
2021-03-14 09:30:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var superTimebox =$(this);
 var superTimeboxID = superTimebox.attr('id');
 superTimeboxID = parseInt(superTimeboxID);
 var currentHour = moment().hour();
+var saveBtns = $('.saveBtns');
+  
 console.log('THE ID OF TH SUPER TIME BLOCK IS', superTimeboxID);
 console.log('The Current Hour IS', currentHour)
 
