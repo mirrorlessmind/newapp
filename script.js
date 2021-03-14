bb94e290c74d5b5f97221eac577b58a355499d90
Cigi <cigi@mirrorlessmind.com>
2021-03-13 22:21:01 -0600

    Updated js script
    starting the variables

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+//Cigi Tipton Schedule
+var timebox = $('.time-box');
+timebox.each(function() {
+var superTimebox =$(this);
+var superTimeboxID = superTimebox.attr('id');
+superTimeboxID = parseInt(superTimeboxID);
+var currentHour = moment().hour();
+console.log('THE ID OF TH SUPER TIME BLOCK IS', superTimeboxID);
+console.log('The Current Hour IS', currentHour)
 
+}
+             
