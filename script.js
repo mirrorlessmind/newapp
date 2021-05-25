f66a6c927c938d9567d5302cb7b9be04b5e2d6f4
Cigi <cigi@mirrorlessmind.com>
2021-05-25 12:35:40 -0500

    updated code

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 //Cigi Tipton Schedule
 //Help from TA
-let timeStamp = $("#curentDay");
+
+let saveButton = $(".btn");
+
+//Moment used to display current day on top.
+let timeStamp = $("#currentDay");
 timeStamp.text(moment().format("dddd, MMMM Do, YYYY"));
-let timeColor + $(".timeblock");
-var getTime = (moment().gormat("H"));
 
-var timebox = $('.time-box');
-timebox.each(function() {
-var superTimebox =$(this);
-var superTimeboxID = superTimebox.attr('id');
-superTimeboxID = parseInt(superTimeboxID);
-var currentHour = moment().hour();
-var saveBtns = $('.saveBtns');
-  
-console.log('THE ID OF TH SUPER TIME BLOCK IS', superTimeboxID);
-console.log('The Current Hour IS', currentHour)
+//added color coding to time blocks
+let timeColor = $(".timeblock");
+var getTime = (moment().format("H"));
 
+console.log(getTime);
+
+function colorCode () {
+    for (let id=8; id<18; id++) {
+        let colorBlock = $("#" + id).children(".planner").text(localStorage.getItem(id));
+        if (id == getTime) {
+            colorBlock.addClass("present")
+        }
+        else if (id < getTime) {
+            colorBlock.addClass("past")
+        }
+        else if (id > getTime) {
+            colorBlock.addClass("future")
+        }
+    }
 }
-             
+
+colorCode();
+
+//events saved to local storage
+function saveEvent(event) {
+    event.preventDefault();
+    var eventInput = $(event.target).parent().children(".planner");
+
+    console.log(eventInput.val());
+    var timeSlot = $(event.target).parent().attr("id");
+    console.log(timeSlot);
+    localStorage.setItem(timeSlot, eventInput.val());
+}
+
+
+
+saveButton.on("click", saveEvent)
